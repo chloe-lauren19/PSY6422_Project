09c68a36b09e8fd84c7bb97ce4d8ba891e618389
Chloe Greenwood <chloegreenwood@MacBookAir.Home>
2025-12-09 11:53:15 +0000

    Updated R scripts

---
+++ R_scripts/000_master.R
@@ -0,0 +1,26 @@
+# Master Script ---------------------------------------------
+## Description :: loads all packages and scripts needed for -
+## this project
+
+# Packages --------------------------------------------------
+
+## Check if packages are installed, if not, install them
+check_packages("tidyverse")
+check_packages("here")
+
+## Load packages
+library(tidyverse)
+library(here)
+
+# Settings --------------------------------------------------
+
+## Set seed to ensure results are reproducible if random -
+## generators are used
+set.seed(1234567)
+
+## Load functions into this project
+source("R_scripts/001_functions.R")
+
+# Processing ------------------------------------------------
+
+## Load in all scripts for this project
